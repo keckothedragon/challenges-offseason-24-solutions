6b7a170131d84d76e3d0e3730ddeeb01c90cbc94
keckothedragon <flappystar05@gmail.com>
2024-09-10 23:09:26 -0400

    Solution 2

--- src/main/java/frc/robot/subsystems/arm/ArmIOKraken.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOKraken.java
@@ -1,37 +1,36 @@
 package frc.robot.subsystems.arm;
 
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 
 public class ArmIOKraken implements ArmIO {
-  // For instructions on how to implement this class, refer to the README.md file
   private TalonFX m_motor;
 
   public ArmIOKraken(int port) {
-    // TODO: Implement this method
+    m_motor = new TalonFX(port);
   }
 
   @Override
   public void setVoltage(double voltage) {
-    // TODO: Implement this method
+    m_motor.setControl(new VoltageOut(voltage));
   }
 
   @Override
   public double getVoltage() {
-    // TODO: Implement this method
-    return 0.0;
+    return m_motor.getMotorVoltage().getValue();
   }
 
   @Override
   public double getVelocityRadiansPerSecond() {
-    // TODO: Implement this method
-    return 0.0;
+    // convert rotations per sec to rad per sec
+    return Units.rotationsToRadians(m_motor.getVelocity().getValue());
   }
 
   @Override
   public Rotation2d getPosition() {
-    // TODO: Implement this method
-    return null;
+    return Rotation2d.fromRotations(m_motor.getPosition().getValue());
   }
 
   @Override
