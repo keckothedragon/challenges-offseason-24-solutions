a097def4ab5af9e4dd3b322e33c72940e903c732
keckothedragon <flappystar05@gmail.com>
2024-09-10 23:20:27 -0400

    Solution 4

--- src/main/java/frc/robot/subsystems/wrist/Wrist.java
+++ src/main/java/frc/robot/subsystems/wrist/Wrist.java
@@ -2,37 +2,53 @@ package frc.robot.subsystems.wrist;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Wrist extends SubsystemBase {
-  // For instructions on how to implement this class, refer to the README.md file
+  private WristIO m_io;
+  private PIDController m_controller;
+  public final WristInputsAutoLogged m_inputs;
+
+  final double TOLERANCE_DEG = 3;
 
   public Wrist(WristIO io, PIDController controller) {
-    // TODO: Implement the constructor
+    m_io = io;
+    m_controller = controller;
+    m_inputs = new WristInputsAutoLogged();
+
+    m_controller.setTolerance(TOLERANCE_DEG);
   }
 
   @Override
   public void periodic() {
-    // TODO: Implement this method
+    m_io.updateInputs(m_inputs);
+
+    // since we store setpoint in the controller we only need to pass one value to calculate()
+    // we use degrees as detailed in the README
+    // (the PID values are tuned for degrees so radians dont work)
+    double pidVoltage = m_controller.calculate(Units.radiansToDegrees(m_inputs.angleRad));
+    m_io.setVoltage(pidVoltage);
   }
 
   public void setDesiredAngle(Rotation2d angle) {
-    // TODO: Implement this method
+    // you can store the desired angle in the pid controller or as its own variable
+    m_controller.setSetpoint(angle.getDegrees());
   }
 
   public Command setDesiredAngleCommand(Rotation2d angle) {
-    // TODO: Implement this method
-    return null;
+    return Commands.runOnce(() -> setDesiredAngle(angle));
   }
 
   public boolean withinTolerance() {
-    // TODO: Implement this method
-    return false;
+    // since the controller knows our current position, setpoint, and tolerance, it knows if we're
+    // within tolerance
+    return m_controller.atSetpoint();
   }
 
   public WristInputsAutoLogged getInputs() {
-    // TODO: Implement this method
-    return null;
+    return m_inputs;
   }
 }
