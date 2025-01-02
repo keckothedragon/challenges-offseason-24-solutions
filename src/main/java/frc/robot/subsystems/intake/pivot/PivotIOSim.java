3549a3af591fea6d1b0a4c9f171b605cedf84b2d
keckothedragon <flappystar05@gmail.com>
2025-01-02 12:47:19 -0500

    Update vendordeps and add comments

--- src/main/java/frc/robot/subsystems/intake/pivot/PivotIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/pivot/PivotIOSim.java
@@ -7,12 +7,14 @@ import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import frc.robot.Constants.IntakeConstants;
 
 public class PivotIOSim implements PivotIO {
+  // use SingleJointedArmSim as detailed in README
   private SingleJointedArmSim m_sim;
 
   // store voltage as variable since sim doesn't have method to get voltage
   private double m_voltage;
 
   public PivotIOSim() {
+    // more physical constants from IntakeConstants and NEO as detailed in README
     DCMotor gearbox = DCMotor.getNEO(1);
     double gearing = IntakeConstants.kPivotGearing;
     double jKgMetersSquared = IntakeConstants.kPivotJKgMetersSquared;
@@ -45,22 +47,26 @@ public class PivotIOSim implements PivotIO {
 
   @Override
   public void setVoltage(double voltage) {
+    // set input voltage and store it for later
     m_sim.setInputVoltage(voltage);
     m_voltage = voltage;
   }
 
   @Override
   public double getVoltage() {
+    // self-explanatory
     return m_voltage;
   }
 
   @Override
   public double getVelocityRadPerSec() {
+    // also self-explanatory
     return m_sim.getVelocityRadPerSec();
   }
 
   @Override
   public Rotation2d getAngle() {
+    // pretty simple
     return Rotation2d.fromRadians(m_sim.getAngleRads());
   }
 }
--- src/main/java/frc/robot/subsystems/intake/rollers/RollerIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/rollers/RollerIOSim.java
@@ -5,12 +5,14 @@ import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.robot.Constants.IntakeConstants;
 
 public class RollerIOSim implements RollerIO {
+  // use DCMotorSim as detailed in README
   private DCMotorSim m_sim;
 
   // store voltage as variable since sim doesn't have method to get voltage
   private double m_voltage;
 
   public RollerIOSim() {
+    // use physical constants from IntakeConstants and kraken x60 as detailed in README
     DCMotor gearbox = DCMotor.getKrakenX60(1);
     double gearing = IntakeConstants.kRollerGearing;
     double jKgMetersSquared = IntakeConstants.kRollerJKgMetersSquared;
@@ -28,17 +30,20 @@ public class RollerIOSim implements RollerIO {
 
   @Override
   public void setVoltage(double voltage) {
+    // set input voltage and store it for later
     m_sim.setInputVoltage(voltage);
     m_voltage = voltage;
   }
 
   @Override
   public double getVoltage() {
+    // self-explanatory
     return m_voltage;
   }
 
   @Override
   public double getVelocityRadPerSec() {
+    // also self-explanatory
     return m_sim.getAngularVelocityRadPerSec();
   }
 }
